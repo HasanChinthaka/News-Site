35883fe469cb0359f924b7b90334a85d365f8084
Hasan Chinthaka <pgrhasanchinthaka@gmail.com>
2025-06-04 06:15:33 +0530

    Update NewsBoard.jsx

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -9,15 +9,14 @@ const NewsBoard = ({ category, language}) => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    useEffect(() => {    
-      api_key = "a2839d44b4fa44239579d0da69749d1d"    
+    useEffect(() => {        
     const getArticles = async () => {
       setIsLoading(true);
       setError(null);
 
       try {
         // Base URL
-        let url = `https://newsapi.org/v2/everything?apiKey=${api_key}`;
+        let url = `https://newsapi.org/v2/everything?apiKey=a2839d44b4fa44239579d0da69749d1d`;
 
         // Add category if not 'all'
         if (category && category !== "general") {
